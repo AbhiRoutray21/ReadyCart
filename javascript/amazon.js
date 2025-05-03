1c52fbde9355f641b44546295e19cfec99cb2e0d
abhijit <routrayabhijit4@gmail.com>
2025-05-03 21:42:36 +0530

    file management

--- javascript/amazon.js
+++ javascript/amazon.js
@@ -1,6 +1,6 @@
 import {addToCart,showCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js'
-import {formatCurrancy} from "../utils/money.js"
+import {formatCurrancy} from "./utils/money.js"
 
 let productsHTML = '';    
 products.forEach((product) =>{
